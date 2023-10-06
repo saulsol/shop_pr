53b1a76da1349c55bc736d51b287000360485253
saul <dlathf3210@naver.com>
2023-10-06 09:47:01 +0900

    [START]

---
+++ src/main/java/com/example/shop_pr_new/ShopPrNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop_pr_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPrNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopPrNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shop_pr_new/ShopPrNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop_pr_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPrNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
